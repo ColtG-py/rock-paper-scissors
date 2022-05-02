03f792306b27593c36e007d19633f379baadf6b1
Colt Gainey <colt.gainey@bracari.com>
2022-05-02 08:05:57 -0400

    pushing bogus commit to test failure

--- src/main/java/com/mcnz/rps/Game.java
+++ src/main/java/com/mcnz/rps/Game.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 
 public class Game {
 	
-	public static void main(String[] args) {
+	public static void ain(String[] args) {
 		String prompt = "Will it be rock, paper or scissors? **";
 		String input = JOptionPane.showInputDialog(prompt);
 		System.out.println("You selected: " + input);
